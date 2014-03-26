0eccda42ab0bd12050784d09e31273f754a6954f
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2014-03-26 06:15:53 -0300

    Obra tiene una chequera propia, que es donde van los cheques emitidos

--- app/models/obra.rb
+++ app/models/obra.rb
@@ -1,6 +1,8 @@
 # encoding: utf-8
 class Obra < ActiveRecord::Base
   has_many :cajas
+  has_one :chequera_propia, ->{ where(tipo: 'Chequera propia') },
+    class_name: 'Caja'
 
   after_create :crear_cajas
 
@@ -15,5 +17,6 @@ class Obra < ActiveRecord::Base
 
       cajas.create tipo: 'Caja de Ahorro', situacion: 'banco'
       cajas.create tipo: 'Chequera', situacion: 'chequera'
+      cajas.create tipo: 'Chequera propia', situacion: 'chequera'
     end
 end
