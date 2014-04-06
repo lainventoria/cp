6393404713c0cfe80b87ee027b6a7b94e9a44cd1
matus <matus@riseup.net>
2014-04-06 18:52:21 -0300

    listado de recibos ajuste visibilidad de columnas segun controlador

--- app/helpers/recibos_helper.rb
+++ app/helpers/recibos_helper.rb
@@ -67,4 +67,23 @@ module RecibosHelper
   def cuentas_de_donde_emitir
     @factura.obra.cajas.cuentas
   end
+
+  def establecer_parametros_listado
+    case params[:controller]
+      when 'recibos'
+        lista_larga = ''
+        lista_causa = 'hidden'
+        muestra_importe = ''
+      when 'facturas'
+        lista_larga = 'hidden'
+        lista_causa = 'hidden'
+        muestra_importe = ''
+      # por eliminacion, esto es para las causas que listan recibos  
+      else
+        lista_larga = 'hidden'
+        lista_causa = ''
+        muestra_importe = 'hidden'
+    end
+    return [ lista_larga, lista_causa, muestra_importe ]
+  end
 end
